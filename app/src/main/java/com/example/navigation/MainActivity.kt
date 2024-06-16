cadba1c388c7ebed3604cfd9bab1e5041870e666
MohamedMohamed <m01126165016@icloud.com>
2024-06-17 02:47:14 +0300

    Integrate action bar with navigation controllerr

--- app/src/main/java/com/example/navigation/MainActivity.kt
+++ app/src/main/java/com/example/navigation/MainActivity.kt
@@ -1,20 +1,32 @@
-package com.example.navigation
+package com.example.mynavigationapp
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
+import com.example.navigation.R
+import com.example.navigation.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        val navController = findNavController(R.id.fragment_host)
+        NavigationUI.setupActionBarWithNavController(this, navController)
+
+        binding.bottomNavigationView.setupWithNavController(navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.fragment_host)
+        navController.navigateUp()
+        return true
     }
 }
