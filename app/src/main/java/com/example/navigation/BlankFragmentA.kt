5dbcbd302e089ef423660497447d38ed44c05902
MohamedMohamed <m01126165016@icloud.com>
2024-06-17 02:46:04 +0300

    Navigate using android navigation component

--- app/src/main/java/com/example/navigation/BlankFragmentA.kt
+++ app/src/main/java/com/example/navigation/BlankFragmentA.kt
@@ -1,59 +1,38 @@
-package com.example.navigation
+package com.example.mynavigationapp
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
+import com.example.navigation.R
+import com.example.navigation.databinding.FragmentBlankABinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [BlankFragmentA.newInstance] factory method to
- * create an instance of this fragment.
- */
-class BlankFragmentA : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
-
+class AFragment : Fragment() {
+    lateinit var binding: FragmentBlankABinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_blank_a, container, false)
+        binding = FragmentBlankABinding.inflate(layoutInflater)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment BlankFragmentA.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            BlankFragmentA().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.buttonB.setOnClickListener { v ->
+            val action = AFragmentDirections.actionAFragmentToBFragment("Hello World")
+            Navigation.findNavController(v).navigate(action)
+
+        }
+
+        binding.buttonC.setOnClickListener { v ->
+            Navigation.findNavController(v).navigate(R.id.action_AFragment_to_CFragment)
+        }
     }
+
+
 }
--- app/src/main/java/com/example/navigation/BlankFragmentC.kt
+++ app/src/main/java/com/example/navigation/BlankFragmentC.kt
@@ -1,59 +1,28 @@
-package com.example.navigation
+package com.example.mynavigationapp
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
+import com.example.navigation.R
+import com.example.navigation.databinding.FragmentBlankCBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [BlankFragmentC.newInstance] factory method to
- * create an instance of this fragment.
- */
-class BlankFragmentC : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
-
+class CFragment : Fragment() {
+    lateinit var binding: FragmentBlankCBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_blank_c, container, false)
+        binding = FragmentBlankCBinding.inflate(layoutInflater)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment BlankFragmentC.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            BlankFragmentC().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.buttonD.setOnClickListener { v ->
+            Navigation.findNavController(v).navigate(R.id.action_CFragment_to_DFragment)
+        }
     }
 }
